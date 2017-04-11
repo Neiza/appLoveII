93627766d38c800640d01f66fc5f6095c11f246c
Neiza Nunez <neizapf@gmail.com>
2017-04-11 06:31:10 -0500

    segundo commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var workImages = document.getElementsByClassName("work-img");
+var modalContainer = document.getElementById("modalContainer");
+var img = document.getElementById("modalImg");
+var close = document.getElementById("close");
+
+function modal(){
+  modalContainer.style.display = "block";
+  img.src = this.src;
+
+}
+function closer(){
+  modalContainer.style.display = "none";
+}
+
+window.addEventListener("load", function(){
+  for (var i=0; i<workImages.length; i++){
+    workImages[i].addEventListener("click",modal)
+  }
+  close.addEventListener("click", function(event){
+    event.preventDefault();
+    closer();
+  });
+});
